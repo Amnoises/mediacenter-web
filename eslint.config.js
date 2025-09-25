aa169278e09aaa1249a65892b5c6a5f1aac53ffa
Dennis <dennis.mende@gmail.com>
2025-09-25 13:51:26 +0200

    chore: enable eslint typescript support

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,36 @@
 import js from '@eslint/js'
 import pluginVue from 'eslint-plugin-vue'
 import globals from 'globals'
+import tseslint from 'typescript-eslint'
 
-export default [
+export default tseslint.config(
   {
-    files: ['**/*.{js,mjs,cjs,jsx,vue}'],
+    ignores: ['dist'],
   },
   {
+    files: ['**/*.{js,mjs,cjs,jsx,ts,tsx,vue}'],
+    extends: [
+      js.configs.recommended,
+      ...pluginVue.configs['flat/essential'],
+      ...tseslint.configs.recommended,
+    ],
     languageOptions: {
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        extraFileExtensions: ['.vue'],
+      },
       globals: {
         ...globals.browser,
       },
     },
   },
-  js.configs.recommended,
-  ...pluginVue.configs['flat/essential'],
-]
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      parserOptions: {
+        parser: tseslint.parser,
+      },
+    },
+  },
+)
